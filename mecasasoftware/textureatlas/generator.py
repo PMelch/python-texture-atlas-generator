e592963761273869a76d2f2a713cc26715ab08cb
peter <peter@peter-NC10.(none)>
2012-11-25 12:18:50 +0100

    bla

--- mecasasoftware/textureatlas/generator.py
+++ mecasasoftware/textureatlas/generator.py
@@ -5,7 +5,18 @@ Created on 25.11.2012
 '''
 
 class Generator(object):
-    pass
+
+
+    def __init__(self):
+        pass
+    
+    def create(self, root_folder):
+        self._groups = dict()
+        for folder, _, filenames in os.walk(root_folder):
+            
+             
+
+
 
 if __name__ == '__main__':
     pass
